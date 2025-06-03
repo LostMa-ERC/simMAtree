ac599fec418cc4ae086f86bae842f87df50a387d
Theo <theo.moins@gmail.com>
2025-06-03 16:57:17 +0200

    Fixing issue on generator init

--- src/generator/__init__.py
+++ src/generator/__init__.py
@@ -1,6 +1,5 @@
 from .base_generator import BaseGenerator
-from .birth_death_tree import BirthDeathTree
 from .birth_death_witness import BirthDeathWitness
 from .yule_witness import YuleWitness
 
-__all__ = ["BaseGenerator", "BirthDeathTree", "BirthDeathWitness", "YuleWitness"]
+__all__ = ["BaseGenerator", "BirthDeathWitness", "YuleWitness"]
